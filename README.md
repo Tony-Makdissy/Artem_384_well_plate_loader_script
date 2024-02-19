395c3c23cef18b5bfa250504fd8b84330e74900f
Tony MAKDISSY <tony.makdissy.sy@gmail.com>
2024-02-19 13:46:16 +0100

    a working code wih draft documentation, not tested

---
+++ README.md
@@ -0,0 +1,31 @@
+# Echo 550 Liquid Handler Transfer Table Generator
+
+This script is used to generate a transfer table for the Echo 550 liquid handler. It reads an XLSX file and generates a CSV file that contains the transfer table.
+
+## Usage
+
+1. Set the configuration parameters at the beginning of the script to match your requirements. These parameters include the default names for the source and destination plates, the default transfer volume, and the number of source wells per destination well.
+
+2. Specify the path to your XLSX file in the `xlsx_file_path` variable.
+
+3. Run the script. The script will read the XLSX file, generate the transfer table, and save it as a CSV file named "transfer_table.csv".
+
+## Input
+
+The input to the script is an XLSX file that contains two sheets, "Source plate Map" and "Destination plate Map". Both sheets should start at C2 and resemble a 384-well plate.
+
+The "Source plate Map" sheet should contain the names of the source wells, and the "Destination plate Map" sheet should contain the names of the destination wells.
+
+## Output
+
+The output of the script is a CSV file named "transfer_table.csv". This file contains the following columns:
+
+- Source Plate: the name of the source plate
+- Source Well: the name of the source well
+- Destination Plate: the name of the destination plate
+- Destination Well: the name of the destination well
+- Transfer Volume: the volume to be transferred from the source well to the destination well
+
+## Notes
+
+If you set the `number_of_source_wells` variable to a specific number (other than -1), the script will raise an error if the number of source wells for a destination well does not match this number. To allow an arbitrary number of source wells, set `number_of_source_wells` to `-1`.
--- copying_script.py
+++ copying_script.py
@@ -1,17 +1,49 @@
+"""
+Written by: Tony MAKDISSY, Artem DEGTIAR
+Last update: 2024-02-15 
+
+This script is used to generate a transfer table for the Echo 550 liquid handler.
+The script reads an XLSX file and generates a CSV file that contains the transfer table.
+The XLSX file should contain two sheets, "Source plate Map" and "Destination plate Map".
+
+The output CSV file contains the following columns:
+- Source Plate: the name of the source plate
+- Source Well: the name of the source well
+- Destination Plate: the name of the destination plate
+- Destination Well: the name of the destination well
+- Transfer Volume: the volume to be transferred from the source well to the destination well
+
+NOTES FOR USAGE:
+you have to manually set the first two blocks of the code to match your file
+- Configratraion
+- Read the xlsx file, MIGHT NEED TO TWEEK IT FOR YOUR OWN FILE
+"""
+
 import pandas as pd
 
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+######  Configratraion ########
+source_plate_default = "Source1"  # Default name for the source plate
+destination_plate_default = "Dest1"  # Default name for the destination plate
+transfer_volume_default = 500  # Default transfer volume
+number_of_source_wells = 2  # Number of source wells per destination well. Set to -1 for arbitrary number of source wells
+############################
 
-
+##### Read the xlsx file, MIGHT NEED TO TWEEK IT FOR YOUR OWN FILE ######
 # Specify the path to your XLSX file
 xlsx_file_path = "AD 03-19 primers.xlsx"
 
 # Read the XLSX file and access the "Source plate Map" sheet
 source_plate = pd.read_excel(xlsx_file_path, sheet_name=2, skiprows=1, usecols="C:Z", nrows=19)
 destination_plate = pd.read_excel(xlsx_file_path, sheet_name=3, skiprows=1, usecols="C:Z", nrows=19)
+# both sheets start at C2, and resemble a 384 well plate
+###########################################################################
 
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def get_source_well(destination_well):
+    """
+    Get the source wells corresponding to a destination well.
+    """
     list_of_source_wells = [x.strip() for x in destination_well.split("+")]
     source_wells = []
 
@@ -22,10 +54,12 @@ def get_source_well(destination_well):
             if type(source_plate.iloc[i, j]) != str:
                 continue
 
-
             if source_plate.iloc[i, j].strip() in list_of_source_wells:
                 source_wells.append(f"{ALPHABET[i]}{j+1}")
     return source_wells
+
+transfer_table_columns = ["Source Plate", "Source Well", "Destination Plate", "Destination Well", "Transfer Volume"]
+transfer_table = pd.DataFrame(columns=transfer_table_columns)
 
 for i in range(destination_plate.shape[0]):
     for j in range(destination_plate.shape[1]):
@@ -34,6 +68,18 @@ for i in range(destination_plate.shape[0]):
         if type(destination_plate.iloc[i, j]) != str:
             continue
         
-        temp = get_source_well(destination_plate.iloc[i, j])
-        if len(temp) != 2:
-            print(temp,destination_plate.iloc[i, j])
+        source_wells = get_source_well(destination_plate.iloc[i, j])
+
+        if number_of_source_wells != -1 and len(source_wells) != number_of_source_wells:
+            raise ValueError(f"You set the number of source wells for each destination well to be {number_of_source_wells}, but the number of source wells for the destination well {destination_plate.iloc[i, j]} is {len(source_wells)}. Please check the source plate map or set the number_of_source_wells to -1 to allow an arbitrary number of source wells.")
+
+        for source_well in source_wells:
+            transfer_table = transfer_table._append({
+                "Source Plate": source_plate_default,
+                "Source Well": source_well,
+                "Destination Plate": destination_plate_default,
+                "Destination Well": f"{ALPHABET[i]}{j+1}",
+                "Transfer Volume": transfer_volume_default
+            }, ignore_index=True)
+
+transfer_table.to_csv("transfer_table.csv", index=False)
